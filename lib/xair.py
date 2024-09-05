10745adc8c586ef4f5c658e3fd8ecf37394526bc
Ross Dickson <ross.m.dickson@gmail.com>
2024-09-05 16:54:11 -0400

    finished recreating UI. Connected Quit and XAir Connect buttons, added record button. Begin recreation of xair connection

---
+++ lib/xair.py
@@ -0,0 +1,150 @@
+"This modules managed communications with the XAir mixer"
+# part of xair-remote.py
+# Copyright (c) 2018, 2021 Peter Dikant
+# Additions Copyright (c) 2021 Ross Dickson
+# Some rights reserved. See LICENSE.
+
+import time
+import threading
+import socket
+import netifaces
+from pythonosc.dispatcher import Dispatcher
+from pythonosc.osc_server import BlockingOSCUDPServer
+from pythonosc.osc_message import OscMessage
+from pythonosc.osc_message_builder import OscMessageBuilder
+
+class OSCClientServer(BlockingOSCUDPServer):
+    "The OSC communications agent"
+    def __init__(self, address, dispatcher):
+        super().__init__(('', 0), dispatcher)
+        self.xr_address = address
+
+    def send_message(self, address, value):
+        "Packs a message for sending via OSC over UDB."
+        builder = OscMessageBuilder(address=address)
+        if value is None:
+            values = []
+        elif isinstance(value, list):
+            values = value
+        else:
+            values = [value]
+        for val in values:
+            builder.add_arg(val)
+        msg = builder.build()
+        self.socket.sendto(msg.dgram, self.xr_address)
+
+class XAirClient:
+    """
+    Handles the communication with the X-Air mixer via the OSC protocol
+    """
+    _CONNECT_TIMEOUT = 0.5
+    _WAIT_TIME = 0.002
+    _REFRESH_TIMEOUT = 5
+
+    XAIR_PORT = 10024
+
+    info_response = []
+
+    def __init__(self, address, state):
+        self.state = state
+        dispatcher = Dispatcher()
+        dispatcher.set_default_handler(self.msg_handler)
+        self.server = OSCClientServer((address, self.XAIR_PORT), dispatcher)
+        worker = threading.Thread(target=self.run_server)
+        worker.daemon = True
+        worker.start()
+
+    def start_connection(self):
+        "Confirm that the connection to the XAir is live, otherwise initiaties shutdown."
+        self.send('/xinfo')
+        time.sleep(self._CONNECT_TIMEOUT)
+        if len(self.info_response) > 0:
+            print('Successfully connected to %s with firmware %s at %s.' % (self.info_response[2],
+                    self.info_response[3], self.info_response[0]))
+        else:
+            print('Error: Failed to setup OSC connection to mixer.',
+                  'Please check for correct ip address.')
+            self.state.quit_called = True
+            if self.server is not None:
+                self.server.shutdown()
+                self.server = None
+
+    def run_server(self):
+        "Start the OSC communications agent in a seperate thread."
+        try:
+            self.server.serve_forever()
+        except KeyboardInterrupt:
+            self.stop_server()
+
+    def stop_server(self):
+        if self.server is not None:
+            self.server.shutdown()
+            self.server = None
+
+    def msg_handler(self, addr, *data):
+        "Dispatch received OSC messages based on message type."
+        if self.state is None or self.state.quit_called:
+            self.stop_server()
+            return
+        #print 'OSCReceived("%s", %s, %s)' % (addr, tags, data)
+        if addr.endswith('/fader') or addr.endswith('/on') or addr.endswith('/level') or \
+                addr.startswith('/config/mute') or addr.endswith('/gain') or addr.startswith('/fx/'):
+            self.state.received_osc(addr, data[0])
+        elif addr == '/xinfo':
+            self.info_response = data[:]
+        elif addr.startswith('/meters'):
+            self.state.received_meters(addr, data)
+        elif addr.startswith('/-'):
+            pass
+        else:         #if self.state.debug and addr.start:
+            print('OSCReceived("%s", %s)' % (addr, data))
+
+    def refresh_connection(self): # the main loop
+        """
+        Tells mixer to send changes in state that have not been received from this OSC Client
+          /xremote        - all parameter changes are broadcast to all active clients (Max 4)
+          /xremotefnb     - No Feed Back. Parameter changes are only sent to the active clients
+                                                                which didn't initiate the change
+        """
+        if self.state.debug:
+            print("Refresh Connection %s" % self.state.levels)
+        try:
+            while not self.state.quit_called and self.server is not None:
+                self.server.send_message("/xremotenfb", None)
+                self.send(address="/meters", param=["/meters/2"])
+                time.sleep(self._REFRESH_TIMEOUT)
+                if self.state.quit_called:
+                    self.stop_server()
+                    return
+        except socket.error:
+            self.stop_server()
+
+    def send(self, address, param=None):
+        "Call the OSC agent to send a message"
+        self.server.send_message(address, param)
+
+def find_mixer():
+    "Search for the IP address of the XAir mixer"
+    print('Searching for mixer...')
+    client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, True)
+    client.settimeout(5)
+    for iface in netifaces.interfaces():
+        try:
+            bcast = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['broadcast']
+            client.sendto("/xinfo\0\0".encode(), (bcast, XAirClient.XAIR_PORT))
+        except:
+            pass
+    try:
+        response = OscMessage(client.recv(512))
+    except socket.timeout:
+        print('No server found')
+        return None
+    client.close()
+
+    if response.address != '/xinfo':
+        print('Unknown response')
+        return None
+    else:
+        print('Found ' + response.params[2] + ' with firmware ' + response.params[3] + ' on IP ' + response.params[0])
+        return response.params[0]
--- main.py
+++ main.py
@@ -13,14 +13,14 @@ from kivy.core.window import Window
 from random import randint
 from collections import deque
 
-
-class ChannelLabels(BoxLayout):
-    # lebels to place top middle and bottom of the box
-    top_text = StringProperty("Top")
-    mid_text = StringProperty("Mid")
+import os
+from lib.xair import XAirClient, find_mixer
 
 class ChannelData(BoxLayout):
     "Data structure to represent a Channel Meter Display"
+    # labels to place left and right of the box
+    in_text = StringProperty("Top")
+    out_text = StringProperty("Mid")
     # color of the bar graph
     in_color = ListProperty([1, 0, 0, 1])
     out_color = ListProperty([0, 0, .5, 1])
@@ -55,34 +55,88 @@ class PongBall(Widget):
 
 class XRemGUI(Widget):
     ball = ObjectProperty(None)
+    # kivy storage for channels
     channels = ObjectProperty(None)
-    channel_labels = ObjectProperty(None)
-    bus_labels = ObjectProperty(None)
+    # kivy storage for buses
+    buses = ObjectProperty(None)
+    # python storage for channels and buses
     channel_data = []
-    channel_label_data = []
-    names = ("Bass", "Guitar", "Keys", "Keys", "Sax", "Don", "Kick", "Snare",
-             "Raluca", "Ors",  "Itai", "Butch", "Ross", "Guest", "OL", "OR")
-    channel_graphs = [None] * 16
+    # xair connection info
+    xair_address = None
+    xair_client = None
+
+    # setup for recording xair audio
+    rec_proc = None
+    my_env = os.environ.copy()
+    my_env['AUDIODEV'] = 'hw:X18XR18,0'
+    record_command = ['rec', '-q', '--buffer', '262144', '-c', '18', '-b', '24']
+    record_file = ""
  
     def paint_buttons(self):
         # Create the represetnations of the 16 channels strips.
         # There is an overlay of a bar graph.
         for x in range(16):
             self.channel_data.append(ChannelData(in_color = [1,0,0,1],
-                                    in_percent = x/16))
+                                    in_percent = x/16, in_text = f'{x+1}',
+                                    out_text = f'Channel {x+1}'))
             self.channels.add_widget(self.channel_data[x])
-            self.channel_label_data.append(ChannelLabels(top_text = f'{x+1}',
-                                    mid_text = self.names[x]))
-            self.channel_labels.add_widget(self.channel_label_data[x])
 
         for x in range(8):
             self.channel_data.append(ChannelData(in_color = [1,0,0,1], 
-            in_percent = x/8))
-            self.second.add_widget(self.channel_data[16+x]),
-            self.channel_label_data.append(ChannelLabels(top_text = f'{2*x+1}', 
-                                    mid_text = f'{2*x+2}'))
-            self.bus_labels.add_widget(self.channel_label_data[16+x])
-    
+                                    in_percent = x/8, in_text = f'Aux {2*x+1}', 
+                                    out_text = f'Aux {2*x+2}'))
+            self.buses.add_widget(self.channel_data[16+x])
+
+    def connect_mixer(self, state):
+        if state == "down":
+            self.quit_called = False
+            # determine the mixer address
+            if self.xair_address is None:
+                self.xair_address = find_mixer()
+                if self.xair_address is None:
+                    print('Error: Could not find any mixers in network.',
+                        'Using default ip address.')
+                    self.xair_address = "192.168.50.146"
+
+            # setup other modules
+            self.xair_client = XAirClient(self.xair_address, self)
+            self.xair_client.start_connection()
+            if self.quit_called:
+                self.xair_client = None
+                return False
+            return True
+        else:
+            if self.xair_client is not None:
+                self.xair_client.stop_server()
+                self.xair_client = None
+
+    def record(self, state):
+        if state == "down":
+            if self.rec_proc is not None:
+                try:
+                    self.record_file = '/media/pi/ExternalSSD/%s.caf' % \
+                        datetime.datetime.now().strftime("%Y-%m-%d-%H:%M:%S")
+                    self.rec_proc = subprocess.Popen(self.record_command + \
+                        [self.record_file], env=self.my_env)
+                except OSError:
+                    if self.rec_proc is not None:
+                        self.rec_proc.terminate()
+        else:
+            if self.rec_proc is not None:
+                self.rec_proc.terminate()
+
+    def quit(self):
+        self.quit_called = True
+        try:
+            if self.rec_proc is not None:
+                self.rec_proc.terminate()
+            if self.xair_client is not None:
+                self.xair_client.stop_server()
+                self.xair_client = None
+            exit()
+        except:
+            exit()
+
     def serve_ball(self):
         self.ball.center = self.center
         self.ball.velocity = Vector(4, 0).rotate(randint(0,360))
@@ -109,8 +163,8 @@ class Xrem(App):
         self.GUI = XRemGUI()
         self.GUI.paint_buttons()
         self.GUI.serve_ball()
-        Clock.schedule_interval(GUI.update, 1.0 / 60.0)
-        return GUI
+        Clock.schedule_interval(self.GUI.update, 1.0 / 60.0)
+        return self.GUI
 
 
 if __name__ == '__main__':
